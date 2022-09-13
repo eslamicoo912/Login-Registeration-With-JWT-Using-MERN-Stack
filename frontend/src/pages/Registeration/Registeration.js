aacfb85ee5c9471f6c132a753771d8321a69f9cf
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-14 00:33:36 +0200

    create the registeration form

--- frontend/src/pages/Registeration/Registeration.js
+++ frontend/src/pages/Registeration/Registeration.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const Registeration = () => {
-  return <div>Registeration</div>;
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    password2: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const handleChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <div className="register text-center">
+      <form onSubmit={handleSubmit}>
+        <h2>Create an account</h2>
+        <input
+          className="form-control w-25 my-4 m-auto"
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          className="form-control w-25 my-4 m-auto"
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="password"
+        />
+        <input
+          className="form-control w-25 my-4 m-auto"
+          type="password"
+          name="password2"
+          value={formData.password2}
+          onChange={handleChange}
+          placeholder="password again"
+        />
+        <button className="btn btn-dark" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Registeration;
