d3f9527cde491086fe7590062847a56cce588975
eslamicoo912 <eslamicoo3@gmail.com>
2022-09-14 00:14:20 +0200

    set up pages and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Registeration from "./pages/Registeration/Registeration";
+import Login from "./pages/Login/Login";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/register" element={<Registeration />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 };
 
 export default App;
